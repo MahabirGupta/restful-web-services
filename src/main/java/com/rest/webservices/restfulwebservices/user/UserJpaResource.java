33534a1933c14688b19cd894c25952026d5f1aad
MahabirGupta <mahabirg81@gmail.com>
2023-10-14 20:34:41 +0800

    Enhance REST API to connect to H2 using JPA and Hibernate by using UserJpaResource.java

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserJpaResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -1,8 +1,8 @@
 package com.rest.webservices.restfulwebservices.user;
 
 //Do a static import of all the methods which are present in WebMvcLinkBuilder
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import com.rest.webservices.restfulwebservices.jpa.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -12,44 +12,46 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-public class UserResource {// Is a REST API
-//We want UserResource to talk to a database through UserRepository
-    private UserDaoService service;
+public class UserJpaResource {// Is a REST API
+//We want UserJpaResource to talk to a database through UserRepository
+    private UserRepository repository;
+
+//    not using UserDaoService service
+//    private UserDaoService service;
 
 //    using constructor injection
 
-    public UserResource(UserDaoService service) {
-        this.service = service;
+    public UserJpaResource(UserRepository repository) {//UserRepository is wired in
+        this.repository = repository;
     }
 
     //    Users REST API
 //    Retrieve all Users
 
 //    GET / all users request
-    @GetMapping("/users") // to get the users from the url
+    @GetMapping("/jpa/users") // to get the users from the database
     public List<User> retrieveAllUsers(){
-            return service.findAll();
+        return repository.findAll();
     }
-
-//    add a link to http://localhost:8080/users
-//    To be able to create a response with data and the links we need to make use of ATS Concepts
-//    EntityModel
-//    WebMvcLinkBuilder
 
     //    GET /specific user request
 
 //    Wrap the User class in EntityModel
 //    A simple EntityModel wrapping a domain object and adding links to it
-    @GetMapping("/users/{id}") // to get the users from the url
+    @GetMapping("/jpa/users/{id}") // to get the users from the url
     public EntityModel<User> retrieveSelectedUser(@PathVariable int id){
-        User user = service.findOne(id);
+        Optional<User> user = repository.findById(id);
 
-        if(user==null){
+        if(user.isEmpty()){
             throw new UserNotFoundException("id:"+id);
         }
-        EntityModel<User> entityModel = EntityModel.of(user);
+        EntityModel<User> entityModel = EntityModel.of(user.get());
 //        Add links to the EntityModel to return links
 //        To create links can use another utility class called WebMvcLinkBuilder
         WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
@@ -59,11 +61,11 @@ public class UserResource {// Is a REST API
 
 //    Create a User
 //    POST /users
-    @PostMapping("/users")
+    @PostMapping("/jpa/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user){//User entity/class
-       User savedUser = service.save(user);
+       User savedUser = repository.save(user);
 
-//        Want to return a url /users/4 of the created resource
+//        Want to return an url /users/4 of the created resource
 //        There is a specific HTTP header called Location Header
 
 //        Want to append /4 to /users/4 => /users/{id}, user.getID
@@ -76,9 +78,9 @@ public class UserResource {// Is a REST API
     }
 
     //    GET /specific user request
-    @DeleteMapping("/users/{id}") // to get the users from the url
+    @DeleteMapping("/jpa/users/{id}") // to get the users from the url
     public void deleteSelectedUser(@PathVariable int id){
-        service.deleteById(id);
+        repository.deleteById(id);
 
     }
 
